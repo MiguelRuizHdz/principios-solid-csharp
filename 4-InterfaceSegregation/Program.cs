69dfc364ce9b36a6a56d0ec71b843e80faf9c375
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2023-04-15 15:48:27 -0600

    Principio de Segregación de Interfaces (ISP)

--- 4-InterfaceSegregation/Program.cs
+++ 4-InterfaceSegregation/Program.cs
@@ -1,3 +1,10 @@
 ﻿using InterfaceSegregation;
 
+/**
+ ************************************ Demo - Escenario ***********************************************
+ * Tenemos una interfaz que implementa múltiples actividades o responsabilidades de diferentes roles *
+ * dentro de un proyecto, se debe aplicar el principio de ISP para distribuir de manera acorde las   *
+ * actividades e implementar solo lo necesario.                                                      *
+ *****************************************************************************************************
+ */
 new Developer().Develop();
